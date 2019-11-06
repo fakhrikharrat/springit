3032b9e1c075be1b46c7a5511507dd693a9c5229
Administrateur <Administrateur@FAKHRI-PC.soretrak.local>
2019-11-06 18:13:54 +0100

    commited

---
+++ springit/src/main/java/com/soretrak/SpringitApplication.java
@@ -0,0 +1,23 @@
+package com.soretrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringitApplication {
+
+	
+	@RequestMapping("/")
+	String home() {
+		return ("Hello Fakhri, This is spring Boot app");
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+}
