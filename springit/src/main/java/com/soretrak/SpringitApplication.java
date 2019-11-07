6c8d3f77b547b165776ed69ab4acb55e19cf4745
Administrateur <Administrateur@FAKHRI-PC.soretrak.local>
2019-11-07 09:54:58 +0100

    welcome message

--- springit/src/main/java/com/soretrak/SpringitApplication.java
+++ springit/src/main/java/com/soretrak/SpringitApplication.java
@@ -18,6 +18,7 @@ public class SpringitApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+		System.out.println("welcome to my springit");
 	}
 
 }
